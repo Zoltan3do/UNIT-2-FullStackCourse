3ec371c49b1260d11faad611fe0490547eeafe9b
Zoltan3do <baronemanuel05@gmail.com>
2024-07-23 15:25:39 +0200

    done

--- 3-JavaScript-3/2-JSON+API_REST/esercitazione/es2.js
+++ 3-JavaScript-3/2-JSON+API_REST/esercitazione/es2.js
@@ -4,15 +4,12 @@ function getRandomColor() {
     return `#${hexColor}`;
 }
 let count = parseInt(sessionStorage.getItem("timer")) || 0;
-let clock = parseInt(sessionStorage.getItem("timer"));
 
 function counter() {
     const secondi = document.getElementById("secondi");
     count++;
-    clock = count;
     sessionStorage.setItem("timer", count);
     secondi.innerText = count;
-    console.log()
     secondi.style.color = getRandomColor();
 }
 
